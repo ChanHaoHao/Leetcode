8886eff7c2445743cafecc0387f3139751e635c8
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2023-01-30 17:03:11 +0800

    Time: 4 ms (22.62%), Space: 43 MB (94.27%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashMap<ListNode, ListNode> map=new HashMap<>();
+
+        while (head!=null) {
+            if (map.containsKey(head) && map.get(head).next==head.next)
+                return true;
+            map.put(head, head);
+            head=head.next;
+        }
+
+        return false;
+    }
+}
