81c74019a0eb440f22171927d4494d4c6b9ee0eb
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2024-01-08 12:01:29 +0800

    Time: 215 ms (24.22%), Space: 19.4 MB (55.12%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        ans, prev_row, current_row = 0, 0, 0
+        for row in bank:
+            row_proccess = row.split("0")
+            for split_row in row_proccess:
+                if split_row != None:
+                    current_row += len(split_row)
+            
+            if current_row==0:
+                continue
+            if prev_row!=0:
+                ans += prev_row*current_row
+            prev_row = current_row
+            current_row=0
+
+        return ans
