e67c52872efa14c4a2a11b1aac4219477756d30f
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2024-01-24 14:59:50 +0800

    Time: 384 ms (93.78%), Space: 43.5 MB (98.67%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -4,36 +4,45 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-ans = 0
-
-def dfs(root, current_path):
-    global ans
-    if root.val not in current_path:
-        current_path[root.val] = 1
-    else:
-        current_path[root.val] += 1
-    
-    if root.left or root.right:
-        if root.left:
-            dfs(root.left, current_path)
-        if root.right:
-            dfs(root.right, current_path)
-    else:
-        single = False
-        for x in current_path:
-            if current_path[x]%2:
-                if single:
-                    print("got single")
-                    ans -= 1
-                    break
-                single = True
-        ans += 1
-
-    current_path[root.val] -= 1
 
 class Solution:
     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
-        global ans
-        ans = 0
-        dfs(root, dict())
-        return ans
+
+        path = dict()
+        odd = 0
+
+        def dfs(root):
+            nonlocal odd
+            if not root:
+                return 0
+
+            # add the value into the path
+            if root.val not in path:
+                path[root.val] = 1
+            else:
+                path[root.val] += 1
+
+            # count how many odds are there after adding the current node
+            if path[root.val]%2:
+                odd += 1
+            else:
+                odd -= 1
+
+            # if this is the last node of the path
+            if not root.left and not root.right:
+                if odd <= 1:
+                    ans = 1
+                else:
+                    ans = 0
+            else:
+                ans = dfs(root.left) + dfs(root.right)
+
+            # after dealing with the node, remove it from the path
+            if path[root.val]%2:
+                odd -= 1
+            else:
+                odd += 1
+            path[root.val] -= 1
+            return ans
+
+        return dfs(root)
