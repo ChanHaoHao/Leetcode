cb454b36d43438e3c60c4b04b12c46dc2438dc40
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2024-01-25 15:35:23 +0800

    Time: 150 ms (35.37%), Space: 17 MB (58.71%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        # deal with the first and the end pot seperately
+        # if there is no enough space to plant
+        if len(flowerbed)-sum(flowerbed) < n:
+            return False
+        # deal with the situation that the first pot is empty
+        elif flowerbed[0]==0:
+            if len(flowerbed)==1:
+                return True
+            elif flowerbed[1]==0:
+                n -= 1
+                flowerbed[0] = 1
+
+        for x in range(1, len(flowerbed)-1):
+            if n<=0:
+                return True
+            elif sum(flowerbed[x-1:x+2])==0:
+                n -= 1
+                flowerbed[x] = 1
+        
+        # deal with the situation that the last pot is empty
+        if len(flowerbed)>2 and flowerbed[-2]+flowerbed[-1]==0:
+            flowerbed[-1] = 1
+            n -= 1
+
+
+        if n>0:
+            return False
+        else:
+            return True
