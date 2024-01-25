bb7d5e851f495af47e168f555c1cc5f84bb97d28
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2024-01-25 12:30:44 +0800

    Time: 40 ms (51.55%), Space: 16.6 MB (59.39%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,5 +1,8 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        
         ans = ""
         # try all the prefix in the shorter string
         if len(str1)<len(str2):
