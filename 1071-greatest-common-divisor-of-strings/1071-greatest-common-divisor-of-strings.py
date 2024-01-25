3dd4e1df2e497cae8b8e941ae088340426e5cfe0
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2024-01-25 12:29:04 +0800

    Time: 36 ms (73.57%), Space: 16.7 MB (59.39%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,28 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        ans = ""
+        # try all the prefix in the shorter string
+        if len(str1)<len(str2):
+            tmp = str1
+            str1 = str2
+            str2 = tmp
+
+        # check all the prefix in str2
+        for x in range(1, len(str2)+1):
+            # if the len of the prefix can't be a factor of both strings
+            if not len(str2)%x and not len(str1)%x:
+                common_divisor = True
+                # check the str1
+                for y in range(len(str1)//x):
+                    if str1[x*y:x*y+x]!=str2[0:x]:
+                        common_divisor = False
+                        break
+                # if str2[0:x] is a common divisor of str1, check str2
+                if common_divisor:
+                    for y in range(len(str2)//x):
+                        if str2[x*y:x*y+x]!=str2[0:x]:
+                            common_divisor = False
+                            break
+                if common_divisor:
+                    ans = str2[0:x]
+        return ans
