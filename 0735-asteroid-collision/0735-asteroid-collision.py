1e2252c5b6fd01f9de396291f83074f6f2770a78
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2024-03-25 15:11:17 +0800

    Time: 89 ms (24.15%), Space: 17.9 MB (68.44%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,26 +1,61 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        # remain = []
+
+        # for x in asteroids:
+        #     # when there are no asteroids remain
+        #     if len(remain)==0:
+        #         remain.append(x)
+        #         continue
+            
+        #     while len(remain)>0:
+        #         prev = remain.pop()
+        #         # if the two asteroids are in the same direction or they will never collide
+        #         if prev*x>0 or prev<0:
+        #             remain.append(prev)
+        #             remain.append(x)
+        #             break
+        #         # deal with the collisions
+        #         else:
+        #             if abs(prev)>abs(x):
+        #                 remain.append(prev)
+        #                 break
+        #             elif abs(prev)==abs(x):
+        #                 break
+        #             elif len(remain)==0:
+        #                 remain.append(x)
+        #                 break
+
+        # return remain
+
         remain = []
 
         for x in asteroids:
+            # if there are no asteroids remain
             if len(remain)==0:
                 remain.append(x)
-                continue
-            while len(remain)>0:
-                prev = remain.pop()
-                # if the two asteroids are in the same direction or they will never collide
-                if prev*x>0 or prev<0:
-                    remain.append(prev)
-                    remain.append(x)
-                    break
-                # deal with the collisions
-                else:
-                    if abs(prev)>abs(x):
-                        remain.append(prev)
-                        break
-                    elif abs(prev)==abs(x):
-                        break
-                    elif len(remain)==0:
+            else:
+                while True:
+                    # if the asteroids will collide
+                    if remain[-1]>0 and x<0:
+                        collide = remain[-1]+x
+                        # same asteroid size
+                        if collide==0:
+                            remain.pop()
+                            break
+                        # the previous asteroid is smaller than the current one
+                        elif collide<0:
+                            remain.pop()
+                        # the current asteroid is smaller than the previous one
+                        else:
+                            break
+
+                        # if there are no asteroids remain
+                        if len(remain)==0:
+                            remain.append(x)
+                            break
+                    # if they won't collide, add it to the remaining
+                    else:
                         remain.append(x)
                         break
 
