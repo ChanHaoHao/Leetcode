1e89a9564395538a955ef892497c1d1ae82cbfb6
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2024-03-27 16:34:05 +0800

    Time: 186 ms (57.27%), Space: 30.7 MB (32.08%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.py
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.py
@@ -6,22 +6,43 @@
 #         self.right = right
 class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
-        def dfs(root, path):
-            if root is None:
-                if len(path)-1>self.ans:
-                    self.ans = len(path)-1
-                return
+        # my method, use len(path) will greatly slow down the program
+        # def dfs(root, path):
+        #     if root is None:
+        #         if len(path)-1>self.ans:
+        #             self.ans = len(path)-1
+        #         return
             
-            if path[-1]=="l":
-                # if it goes zigzag, keep going, else restart the path
-                dfs(root.right, path+"r")
-                dfs(root.left, "l")
-            else:
-                dfs(root.left, path+"l")
-                dfs(root.right, "r")
+        #     if path[-1]=="l":
+        #         # if it goes zigzag, keep going, else restart the path
+        #         dfs(root.right, path+"r")
+        #         dfs(root.left, "l")
+        #     else:
+        #         dfs(root.left, path+"l")
+        #         dfs(root.right, "r")
+
+        # self.ans = 0
+        # dfs(root.left, "l")
+        # dfs(root.right, "r")
+
+        # return self.ans
 
         self.ans = 0
-        dfs(root.left, "l")
-        dfs(root.right, "r")
+        def dfs(root, dir, size):
+            self.ans = max(self.ans, size)
+
+            if root.left is not None:
+                if dir=="l":
+                    dfs(root.left, "l", 1)
+                else:
+                    dfs(root.left, "l", size+1)
+
+            if root.right is not None:
+                if dir=="r":
+                    dfs(root.right, "r", 1)
+                else:
+                    dfs(root.right, "r", size+1)
+
+        dfs(root, "", 0)
 
         return self.ans
