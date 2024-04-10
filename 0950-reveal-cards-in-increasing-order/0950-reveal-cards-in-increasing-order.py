22f1bb18a16a434cb9b9dde94b08185a8d67c355
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2024-04-10 16:40:21 +0800

    Time: 38 ms (93.72%), Space: 16.8 MB (36.98%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -6,24 +6,44 @@ class Solution:
         # second pass: 1 _ 2 5 3 _ 4
         # third pass: 1 6 2 5 3 7 4
         # every time you input a number, you will have to skip the next available slot
+#         n = len(deck)
+#         result = [0]*n
+#         skip = False
+
+#         indexInDeck = 0
+#         indexInResult = 0
+
+#         deck = sorted(deck)
+
+#         while indexInDeck<n:
+#             # only flipping skip or putting values into result when the current value is 0
+#             if result[indexInResult]==0:
+#                 if not skip:
+#                     result[indexInResult] = deck[indexInDeck]
+#                     indexInDeck+=1
+#                     skip = True
+#                 else:
+#                     skip = False
+
+#             indexInResult = (indexInResult+1)%n
+#         return result
+
+        # even a smarter version, use deque() to perform the input
+        # after popleft of the queue, move the first element of the queue to the back
+        # by using q.append(q.popleft())
         n = len(deck)
-        result = [0]*n
-        skip = False
-
-        indexInDeck = 0
-        indexInResult = 0
-
-        deck = sorted(deck)
-
-        while indexInDeck<n:
-            # only flipping skip or putting values into result when the current value is 0
-            if result[indexInResult]==0:
-                if not skip:
-                    result[indexInResult] = deck[indexInDeck]
-                    indexInDeck+=1
-                    skip = True
-                else:
-                    skip = False
-
-            indexInResult = (indexInResult+1)%n
-        return result
+        q = deque()
+        
+        for x in range(n):
+            q.append(x)
+            
+        deck.sort()
+            
+        results = [0]*n
+        for x in deck:
+            results[q.popleft()] = x
+            
+            if q:
+                q.append(q.popleft())
+                
+        return results
