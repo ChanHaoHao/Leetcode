79bb1d995ea9306fed0b42f61dee33d6623eb5e6
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2024-07-20 18:14:59 +0800

    Time: 450 ms (79.42%), Space: 22.1 MB (7.22%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
@@ -2,39 +2,6 @@ class Solution:
     def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
         m, n = len(rowSum), len(colSum)
         ans = [[-1 for _ in range(n)] for _ in range(m)]
-
-        # thres = min(m, n)
-
-        # for y in range(thres):
-        #     if rowSum[y]>colSum[y]:
-        #         ans[y][y] = colSum[y]
-        #         rowSum[y] -= colSum[y]
-        #         for x in range(m):
-        #             if x!=y:
-        #                 ans[x][y] = 0
-        #     else:
-        #         ans[y][y] = rowSum[y]
-        #         colSum[y] -= rowSum[y]
-        #         for x in range(n):
-        #             if x!=y:
-        #                 ans[y][x] = 0
-
-        # for y in range(m):
-        #     for x in range(n):
-        #         if ans[y][x]==-1:
-        #             if rowSum[y]>colSum[x]:
-        #                 ans[y][x] = colSum[x]
-        #                 rowSum[y] -= colSum[x]
-        #                 for z in range(m):
-        #                     if ans[z][x]==-1:
-        #                         ans[z][x] = 0
-        #             else:
-        #                 ans[y][x] = rowSum[y]
-        #                 colSum[x] -= rowSum[y]
-        #                 for z in range(n):
-        #                     if ans[y][z]==-1:
-        #                         ans[y][z] = 0
-
         row, col = 0, 0
 
         while row<m and col<n:
