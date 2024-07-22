f42ef1d4efa7b3dfa8df7075b05c15b98620425f
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2024-07-23 01:33:38 +0800

    Time: 0 ms (100.00%), Space: 9.5 MB (14.27%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -13,11 +13,14 @@ public:
                 return nums[1];
         }
 
+        // create the candidates for the 3, ..., end houses
         int dp[3] = {nums[0], nums[1], nums[0]+nums[2]};
 
         for (int i=3; i<n; i++)
         {
             int curr = nums[i];
+            // at house N, the theif only has to consider N-2 or N-3 to steal, 
+            // since he can't steal N-1 ,and N-4 will always be less than N-2
             if (dp[0]>dp[1])
                 curr += dp[0];
             else
