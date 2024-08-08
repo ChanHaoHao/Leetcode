db09cd4a8a0be0e71ab0ca70834a6553711b1697
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2024-08-08 16:10:01 +0800

    Time: 73 ms (91.02%), Space: 17.7 MB (96.09%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
@@ -2,30 +2,46 @@ class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
         # greedy method - 
         # after observation, we know that going into spiral we need 1 right, 1 down, 2 left, 2 up, 3 right, etc...
-        ans, count, step = [[rStart, cStart]], rows*cols-1, 1
+        # ans, count, step = [[rStart, cStart]], rows*cols-1, 1
 
-        def in_matrix(r, c):
-            nonlocal count, ans
-            if r>=0 and r<rows and c>=0 and c<cols:
-                count -= 1
-                ans.append([r, c])
+        # def in_matrix(r, c):
+        #     nonlocal count, ans
+        #     if r>=0 and r<rows and c>=0 and c<cols:
+        #         count -= 1
+        #         ans.append([r, c])
 
-        while count:
-            if step%2==1:
-                for x in range(step):
-                    cStart += 1
-                    in_matrix(rStart, cStart)
+        # while count:
+        #     if step%2==1:
+        #         for x in range(step):
+        #             cStart += 1
+        #             in_matrix(rStart, cStart)
                         
-                for x in range(step):
-                    rStart += 1
-                    in_matrix(rStart, cStart)
-            else:
-                for x in range(step):
-                    cStart -= 1
-                    in_matrix(rStart, cStart)
-                for x in range(step):
-                    rStart -= 1
-                    in_matrix(rStart, cStart)
+        #         for x in range(step):
+        #             rStart += 1
+        #             in_matrix(rStart, cStart)
+        #     else:
+        #         for x in range(step):
+        #             cStart -= 1
+        #             in_matrix(rStart, cStart)
+        #         for x in range(step):
+        #             rStart -= 1
+        #             in_matrix(rStart, cStart)
+        #     step += 1
+
+        # return ans
+
+        # same idea but faster method
+        direction = [[0,1], [1,0], [0,-1], [-1,0]]
+        ans, step, i = [], 1, 0
+        while len(ans)<rows*cols:
+            for _ in range(2):
+                dr, dc = direction[i]
+                for _ in range(step):
+                    if 0<=rStart<rows and 0<=cStart<cols:
+                        ans.append([rStart, cStart])
+                    rStart += dr
+                    cStart += dc
+                i = (i+1)%4
             step += 1
 
         return ans
