01f1ae57af1078825c8f1bfc9eb1099f11558cc8
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2025-01-11 21:23:29 -0800

    Time: 0 ms (100%), Space: 17.6 MB (39.7%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,18 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        m-=1
-        n-=1
-        for x in range(len(nums1)-1,-1,-1):
+        # start from m+n-1 and put the largest value of num1 and num2 to the end of num1
+        # if nums1 has used all its numbers, put nums2 in it
+        # if nums2 has used all its numbers, no further action needed
+        m -= 1
+        n -= 1
+
+        for x in range(len(nums1)-1, -1, -1):
             if m>=0 and n>=0:
                 if nums1[m]>nums2[n]:
-                    nums1[x]=nums1[m]
-                    m-=1
+                    nums1[x] = nums1[m]
+                    m -= 1
                 else:
-                    nums1[x]=nums2[n]
-                    n-=1
-                    
+                    nums1[x] = nums2[n]
+                    n -= 1
             else:
                 if m<0:
-                    nums1[x]=nums2[n]
-                    n-=1
+                    nums1[x] = nums2[n]
+                    n -= 1
