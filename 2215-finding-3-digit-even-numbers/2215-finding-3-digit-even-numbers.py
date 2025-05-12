b61e54b28c2422d0ab641af891b9fe6853eacbb9
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2025-05-12 01:07:49 -0400

    Time: 76 ms (54.6%), Space: 19.5 MB (8.07%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.py
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.py
@@ -1,17 +1,22 @@
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        # first store the answer in a set
         ans = set()
+        # count the frequency of the digits
         count = Counter(digits)
 
         def backtrack(num, length):
+            # if it is three digits and an even number
             if length==3 and num%2==0:
                 ans.add(num)
                 return
             
+            # try every valid possibility and see we can add the number or not
             for x in range(10):
                 if count[x]==0 or not count[x]:
                     continue
                 
+                # don't let the number to start with 0
                 if length==0 and x==0:
                     continue
 
