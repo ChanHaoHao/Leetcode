e6ecb883378db3c05fe861a02aa795188cff366e
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2025-10-25 15:22:32 -0400

    Time: 15 ms (97.09%), Space: 23.3 MB (80.51%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,21 +1,17 @@
-import numpy
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        tmp = 1
-        zeros = []
-        ans = []
-        for x in range(len(nums)):
-            if nums[x] != 0:
-                tmp *= nums[x]
-            else:
-                zeros.append(x)
+        ans = [0] * len(nums)
+        ans[0] = 1
+        # Each number will be the multiplication of its precount and postcount
+        # take index=3 for example, it will be the multiplication of 0~2 and 4~end
+        # And 0~2 will be the prefix, and 4~end will be the postfix
+        # So we first iterate through with prefix, and initialize the first number with 1
+        for i in range(len(nums)-1):
+            ans[i+1] = ans[i] * nums[i]
         
-        if len(zeros)>=2:
-            ans = numpy.zeros(len(nums), dtype=int)
-        elif len(zeros)==1:
-            ans = numpy.zeros(len(nums), dtype=int)
-            ans[zeros[0]] = tmp
-        else:
-            for x in range(len(nums)):
-                ans.append(int(tmp/nums[x]))
+        # Then we start to iterate through postfix
+        post_count = nums[-1]
+        for i in range(len(nums)-2, -1, -1):
+            ans[i] *= post_count
+            post_count *= nums[i]
         return ans
