9d075b7916cbef9e82ed41a1b8edd552f29e95ab
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2025-10-25 15:37:22 -0400

    Time: 0 ms (100%), Space: 17.8 MB (60.15%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.py
@@ -3,9 +3,11 @@ class Solution:
         ans = 0
         week = [1, 2, 3, 4, 5, 6, 7]
         sum_week = sum(week)
+        # Deal with full week
         for i in range(n//7):
             ans += sum_week + i*7
 
+        # Deal with rest of the days
         for i in range(n%7):
             ans += week[i] + n//7
 
