67d12bf8640b7689edde2a738a351a69c7e9a96e
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2025-11-06 09:53:05 -0500

    Time: 1 ms (32.21%), Space: 18.2 MB (20.6%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -2,14 +2,16 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         # count how many times a citation has happened
         count = Counter(citations)
-        keys = sorted(count.keys())
         papers = len(citations)
         
         ans = 0
-        for key in range(keys[-1]+1):
+        # go through all possible h-index
+        for key in range(max(count.keys())+1):
+            # if the remaining papers have citations greater than the h-index
             if papers>=key:
                 ans = key
-            if key in keys:
+            # remove the papers less than h-index
+            if key in count.keys():
                 papers -= count[key]
         
         return ans
