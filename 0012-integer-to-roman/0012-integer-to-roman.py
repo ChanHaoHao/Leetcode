a3c5ac43b73a6ea6caa690802515399519f9f2f2
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2025-11-09 21:49:14 -0500

    Time: 4 ms (64.08%), Space: 17.8 MB (68.96%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,20 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        convert = {0: ["I", "V"], 1: ["X", "L"], 2: ["C", "D"], 3: ["M", "M"]}
+
+        ans = "M"*(num//1000)
+        num = num%1000
+        for i in range(3, -1, -1):
+            if num//10**i!=0:
+                curr = num//10**i
+                if 1<=curr<=3:
+                    ans += convert[i][0]*curr
+                elif 5<=curr<=8:
+                    ans += convert[i][1]+convert[i][0]*(curr-5)
+                elif curr==4:
+                    ans += convert[i][0] + convert[i][1]
+                elif curr==9:
+                    ans += convert[i][0] + convert[i+1][0]
+            num = num%10**i
+        
+        return ans
