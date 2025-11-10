5a8ec03ffb704293015587bd5a2ae2b7c218d63b
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2025-11-09 20:52:51 -0500

    Time: 7 ms (42.02%), Space: 17.9 MB (39.8%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        convert = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        before = {"I": ["V", "X"], "X": ["L", "C"], "C": ["D", "M"]}
+
+        ans, n, i = 0, len(s), 0
+        while i<n:
+            if s[i] in before and i+1<n and s[i+1] in before[s[i]]:
+                ans += convert[s[i+1]]-convert[s[i]]
+                i += 2
+            else:
+                ans += convert[s[i]]
+                i += 1
+        return ans
