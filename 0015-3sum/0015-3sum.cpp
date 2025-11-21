37736d1a9f80a9e27683c2534857ea458f1d6bdf
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2025-11-21 11:42:12 -0500

    Time: 48 ms (58.84%), Space: 29.1 MB (47.8%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
+        // sorte the nums
         std::sort(nums.begin(), nums.end());
 
+        // Treat it as a two sum problem
         for (int i=0; i<nums.size()-2; i++)
         {
+            // If the target is the same as the previous index, skip it
             if (i-1>=0 && nums[i]==nums[i-1])
-            {
-                cout << "Duplicate " << nums[i] << "\n";
                 continue;
-            }
 
             int target=-nums[i], l=i+1, r=nums.size()-1;
             while (l<r)
             {
+                // if the goal is found
                 if (nums[l]+nums[r]==target)
                 {
                     ans.push_back(vector<int>{nums[i], nums[l], nums[r]});
                     l++;
                     r--;
+                    // prevent existing same pairs
                     while (l<r && nums[l]==nums[l-1])
                         l++;
                     while (l<r && nums[r]==nums[r+1])
