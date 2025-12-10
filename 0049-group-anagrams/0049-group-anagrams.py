499f4390a70c2b3fd2d5cc942f2b3fd3c4f9526f
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2025-12-10 12:57:25 -0500

    Time: 16 ms (89.88%), Space: 15.9 MB (98.73%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,34 +1,21 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # seperated=[]
-        # for x in strs:
-        #     tmp=sorted(list(x))
-        #     seperated.append(tmp)
-        # found=[]
-        # ans=[]
-        # for x in range(len(strs)-1):
-        #     if x in found:
-        #         continue
-        #     cur=[strs[x]]
-        #     found.append(x)
-        #     tmp=seperated[x]
-        #     for y in range(x+1, len(strs)):
-        #         seperated_tmp=seperated[y]
-        #         if seperated_tmp==tmp:
-        #             cur.append(strs[y])
-        #             found.append(y)
-        #     ans.append(cur)
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
 
-        # if len(strs)-1 not in found:
-        #     ans.append([strs[-1]])
-        # return ans
-        ans=collections.defaultdict(list)
-
-        for x in strs:
-            count=[0]*26
-            for s in x:
-                count[ord(s)-ord('a')]+=1
-                # use the tuple as the index, brilliant thought
-            ans[tuple(count)].append(x)
-
-        return ans.values()
+        ans = {}
+        for s in strs:
+            sorted_s = sorted(s)
+            sorted_s = ''.join(sorted_s)
+            if sorted_s in ans:
+                ans[sorted_s].append(s)
+            else:
+                ans[sorted_s] = [s]
+        
+        res = []
+        for key in ans.keys():
+            res.append(ans[key])
+        return res
+        
