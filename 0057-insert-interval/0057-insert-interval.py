c818dfcaebdc03491ec83fd444d97550bceb421f
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2025-12-25 16:36:14 -0500

    Time: 0 ms (100%), Space: 19.2 MB (94.97%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,31 +1,22 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        i, n = 0, len(intervals)
         ans = []
-
-        merged = False
-        merge = [-1, -1]
-        for interval in intervals:
-            # No overlap with newInterval
-            if interval[1]<newInterval[0]:
-                ans.append(interval)
-            elif interval[0]>newInterval[1]:
-                if not merged:
-                    if merge[0]==-1:
-                        merge[0] = newInterval[0]
-                    if merge[1]==-1:
-                        merge[1] = newInterval[1]
-                    ans.append(merge)
-                    merged = True
-                ans.append(interval)
-            else:
-                if merge[0]==-1:
-                    merge[0] = min(interval[0], newInterval[0])
-                merge[1] = max(interval[1], newInterval[1])
+        # Seperate the intervals into 3 parts
+        # First part: interval[1]<newInterval[0]
+        while i<n and intervals[i][1]<newInterval[0]:
+            ans.append(intervals[i])
+            i += 1
         
-        if not merged:
-            if sum(merge)==-2:
-                ans.append(newInterval)
-            else:
-                ans.append(merge)
+        # Second part: Find the overlapping
+        while i<n and intervals[i][0]<=newInterval[1]:
+            newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+            i += 1
+        ans.append(newInterval)
 
+        # Third part: non-overlap interval[0]>newInterval[1]
+        while i<n:
+            ans.append(intervals[i])
+            i += 1
+        
         return ans
