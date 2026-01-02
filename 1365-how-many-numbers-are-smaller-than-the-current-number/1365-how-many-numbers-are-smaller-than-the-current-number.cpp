84c4a4b72ca1928c999d08f97dd8fea45a05493c
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-01-02 16:31:52 -0500

    Time: 0 ms (100%), Space: 14.6 MB (14.4%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        map<int, int> count;
-        for (int n: nums)
-            count[n]++;
+        // map<int, int> count;
+        // for (int n: nums)
+        //     count[n]++;
 
-        int acc = 0;
-        unordered_map<int, int> accum;
-        for (auto n: count)
+        // int acc = 0;
+        // unordered_map<int, int> accum;
+        // for (auto n: count)
+        // {
+        //     accum[n.first] = acc;
+        //     acc += n.second;
+        // }
+
+        // vector<int> ans(nums.size());
+        // for (int i=0; i<nums.size(); i++)
+        // {
+        //     ans[i] = accum[nums[i]];
+        // }
+
+        // return ans;
+
+        vector<int> sorted=nums;
+        sort(sorted.begin(), sorted.end());
+        unordered_map<int, int> m;
+        for (int i=0; i<nums.size(); i++)
         {
-            accum[n.first] = acc;
-            acc += n.second;
+            // Use the sorted array as index, for how many number is smaller ahead
+            if (m.find(sorted[i])==m.end())
+                m[sorted[i]] = i;
         }
 
         vector<int> ans(nums.size());
         for (int i=0; i<nums.size(); i++)
-        {
-            ans[i] = accum[nums[i]];
-        }
+            ans[i] = m[nums[i]];
 
         return ans;
     }
