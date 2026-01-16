e5a455bbaeae630b050235ec69c0f6f6800cb270
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-01-15 22:47:19 -0500

    Time: 9 ms (39.68%), Space: 23.3 MB (6.78%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,15 +1,32 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals)
-        n = len(intervals)
-        i = 0
-        while i<n-1:
-            if intervals[i][1]>=intervals[i+1][0]:
-                if intervals[i][1] < intervals[i+1][1]:
-                    intervals[i][1] = intervals[i+1][1]
-                intervals.pop(i+1)
-                n = len(intervals)
-            else:
-                i += 1
+        # intervals = sorted(intervals)
+        # n = len(intervals)
+        # i = 0
+        # while i<n-1:
+        #     if intervals[i][1]>=intervals[i+1][0]:
+        #         if intervals[i][1] < intervals[i+1][1]:
+        #             intervals[i][1] = intervals[i+1][1]
+        #         intervals.pop(i+1)
+        #         n = len(intervals)
+        #     else:
+        #         i += 1
 
-        return intervals
+        # return intervals
+        if len(intervals)<2:
+            return intervals
+
+        intervals = sorted(intervals)
+        start, end = intervals[0][0], intervals[0][1]
+
+        overlap = []
+        for i in range(1, len(intervals)):
+            if intervals[i][0]>end:
+                overlap.append([start, end])
+                start = intervals[i][0]
+                end = intervals[i][1]
+            else:
+                end = max(end, intervals[i][1])
+        
+        overlap.append([start, end])
+        return overlap
