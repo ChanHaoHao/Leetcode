2169d1d1e3f88b8c5697c61d2d1fa214cabb8887
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-01-15 21:53:33 -0500

    Time: 44 ms (91.94%), Space: 24.6 MB (13.29%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,17 +1,11 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # if len(points) equals the number of points we need to return
-        if k==len(points):
-            return points
-        distance=[]
-        
-        # save the distance and cordinates in the same list, then heapify it
-        # it will be heapified using the first element of the sublist
-        for cord in points:
-            distance.append([cord[0]*cord[0]+cord[1]*cord[1], cord])
-            
-        heapq.heapify(distance)
-        ans=[]
-        while len(ans)<k:
-            ans.append(heapq.heappop(distance)[1])
+        dist = []
+        for point in points:
+            dist.append((point[0]**2+point[1]**2, point))
+        dist = sorted(dist)
+
+        ans = []
+        for i in range(k):
+            ans.append(dist[i][1])
         return ans
