add7582c703eb46e2b98f684ae4be729bfe60682
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-01-15 21:55:32 -0500

    Time: 43 ms (92.95%), Space: 24.8 MB (11.01%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,5 +1,6 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # heapify dist after it is calculated will be better
         dist = []
         for point in points:
             dist.append((point[0]**2+point[1]**2, point))
