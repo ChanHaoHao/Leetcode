f6c7a65fbf79e8ec68da769ae0ebffa0dadafbbc
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-01-19 15:40:03 -0500

    Time: 179 ms (30.26%), Space: 23.7 MB (21.54%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -5,7 +5,6 @@ class Solution:
             prefix1[i+1] = prefix1[i] + (1 if s[i]=="1" else 0)
         
         res = len(s)
-        flip = []
         for i in range(0, len(s)+1):
             res = min(res, prefix1[i] + (len(s) - i - prefix1[-1]+prefix1[i]))
         return res
