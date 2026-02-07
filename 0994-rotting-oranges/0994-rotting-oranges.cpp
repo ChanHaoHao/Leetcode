dd6e96e29d2baae72eaa33e708377294971448f9
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-07 07:39:52 -0500

    Time: 11 ms (6.57%), Space: 17.5 MB (10.92%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,56 +1,55 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int count = 0, m = grid.size(), n = grid[0].size();
-        int fresh = 0;
-        queue<pair<int, int>> rotten;
+        int ans = 0;
+        int ROW = grid.size(), COL = grid[0].size();
+        int dr[4] = {-1, 1, 0, 0};
+        int dc[4] = {0, 0, -1, 1};
 
-        for (int i=0; i<m; i++)
-        {
-            for (int j=0; j<n; j++)
-            {
-                if (grid[i][j]==2)
-                {
-                    rotten.push(make_pair(i, j));
-                }
-                else if (grid[i][j]==1)
-                {
-                    fresh += 1;
-                }
+        // Use queue to store all rotten orange
+        queue<vector<int>> q;
+        // Find all exist rotten orange
+        for (int r=0; r<ROW; ++r) {
+            for (int c=0; c<COL; ++c) {
+                if (grid[r][c] == 2)
+                    q.push({r, c});
             }
         }
 
-        vector<pair<int, int>> delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        bool changed = false;
-        while (fresh!=0)
-        {
-            count++;
-            int level = rotten.size();
-            for (int i=0; i<level; i++)
-            {
-                pair<int, int> pos = rotten.front();
-                rotten.pop();
-                for(int j=0; j<4; j++)
-                {
-                    int dy = delta[j].first, dx = delta[j].second;
-                    if (pos.first+dy<m && pos.first+dy>-1 && pos.second+dx<n && pos.second+dx>-1)
-                    {
-                        if (grid[pos.first+dy][pos.second+dx]==1)
-                        {
-                            fresh--;
-                            grid[pos.first+dy][pos.second+dx] = 2;
-                            rotten.push(make_pair(pos.first+dy, pos.second+dx));
-                            changed = true;
-                        }
+        // Use BFS to iterate through all oranges that can be infected
+        while (!q.empty()) {
+            // iterate this level
+            int n = q.size();
+            for (n; n>0; --n) {
+                vector<int> curr = q.front();
+                q.pop();
+
+                // Check if there is fresh orange in its neighbors
+                int nxt_r, nxt_c;
+                for (int i=0; i<4; ++i) {
+                    nxt_r = curr[0] + dr[i];
+                    nxt_c = curr[1] + dc[i];
+
+                    // first check if out of bound, then check if it is fresh orange
+                    if (0 <= nxt_r && nxt_r < ROW && 0 <= nxt_c && nxt_c < COL && grid[nxt_r][nxt_c]==1) {
+                        // push it into the queue for bfs
+                        q.push({nxt_r, nxt_c});
+                        // mark it as rotten
+                        grid[nxt_r][nxt_c] = 2;
                     }
                 }
             }
-            if (!changed)
-            {
-                return -1;
-            }
-            changed = false;
+
+            ans++;
         }
-        return count;
+
+        for (int r=0; r<ROW; ++r) {
+            for (int c=0; c<COL; ++c) {
+                if (grid[r][c] == 1)
+                    return -1;
+            }
+        }
+
+        return (ans > 0) ? ans - 1 : ans;
     }
 };
