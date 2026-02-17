8453fdb4e0d6aa00bbdea10ec19e89880f050060
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-17 14:51:08 -0500

    Time: 112 ms (74.86%), Space: 78.8 MB (17.97%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, key, value):
         self.key = key
-        self.value = value
+        self.val = value
         self.prev = self.next = None
 
 class LRUCache:
@@ -9,28 +9,28 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.c = capacity
         self.cache = {}
-        self.left = self.right = Node(0, 0)
+        self.left = Node(0, 0)
+        self.right = Node(0, 0)
         self.left.next = self.right
         self.right.prev = self.left
-
+    
     def remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next = nxt
         nxt.prev = prev
     
     def insert(self, node):
-        prev = self.right.prev
+        prev, nxt = self.right.prev, self.right
         prev.next = node
-        node.next = self.right
-        self.right.prev = node
+        node.next = nxt
+        nxt.prev = node
         node.prev = prev
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            # remove the key from the double linked list, and put at the most recent use
             self.remove(self.cache[key])
             self.insert(self.cache[key])
-            return self.cache[key].value
+            return self.cache[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
