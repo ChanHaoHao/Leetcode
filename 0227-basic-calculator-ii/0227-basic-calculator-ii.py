a8778850b2e9a492a9ce82d2793ce9958df007e9
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-17 10:51:04 -0500

    Time: 61 ms (52.31%), Space: 22.4 MB (45.35%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,43 +1,26 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        # iterate through the string, and deal with * and / first
-        # remove all empty space
-        s = s.replace(" ", "")
+        def update(sign, num):
+            if sign == "+":
+                stack.append(num)
+            elif sign == "-":
+                stack.append(-num)
+            elif sign == "*":
+                stack.append(stack.pop() * num)
+            elif sign == "/":
+                stack.append(int(stack.pop() / num))
 
-        stack = deque()
-        i = 0
+        i, num, stack, sign = 0, 0, [], "+"
+
         while i < len(s):
-            if s[i] == '*' or s[i] == '/':
-                l = i+1
-                # get the second number
-                while (l < len(s) and '0' <= s[l] <= '9'):
-                    l += 1
-                second = int(s[i+1: l])
-                first = stack.pop()
-                if s[i] == '*':
-                    stack.append(first * second)
-                else:
-                    stack.append(first // second)
-                i = l
-            elif s[i] == '+' or s[i] == '-':
-                stack.append(s[i])
-                i += 1
-            else:
-                l = i+1
-                # get the number
-                while (l < len(s) and '0' <= s[l] <= '9'):
-                    l += 1
-                stack.append(int(s[i: l]))
-                i = l
+            if s[i].isdigit():
+                num = num*10 + int(s[i])
+            elif s[i] in "+-*/":
+                update(sign, num)
+                num = 0
+                sign = s[i]
+            i += 1
+            
+        update(sign, num)
 
-        while len(stack) > 2:
-            first = stack.popleft()
-            oper = stack.popleft()
-            second = stack.popleft()
-
-            if oper == '+':
-                stack.appendleft(first + second)
-            else:
-                stack.appendleft(first - second)
-        
-        return stack[0]
+        return sum(stack)
