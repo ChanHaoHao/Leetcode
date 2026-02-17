9ccde0d4e1fde4564cdfd526d749a50da08ce66f
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-17 08:55:13 -0500

    Time: 174 ms (13.33%), Space: 78.8 MB (10.68%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.c = capacity
+        self.cache = {}
+        self.left = self.right = Node(0, 0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    def insert(self, node):
+        prev = self.right.prev
+        prev.next = node
+        node.next = self.right
+        self.right.prev = node
+        node.prev = prev
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            # remove the key from the double linked list, and put at the most recent use
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.c:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
