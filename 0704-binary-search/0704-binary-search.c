03840e505ddf9ad3f798afda8ba1d9a84d0ee269
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-19 11:18:40 -0500

    Time: 0 ms (100%), Space: 9.4 MB (12.56%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.c
@@ -0,0 +1,18 @@
+int search(int* nums, int numsSize, int target) {
+    int l=0, r=numsSize-1;
+
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+        else if (nums[mid] > target) {
+            r = mid - 1;
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+
+    return -1;
+}
