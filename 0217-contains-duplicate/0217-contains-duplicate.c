bb9fe125d6b04471604b7181f6b124594681882a
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-19 18:20:26 -0500

    Time: 60 ms (12.82%), Space: 17.3 MB (54.71%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.c
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -1,5 +1,5 @@
-int compare (const void *a, const void *b) {
-    return (*(int*)a -*(int*)b);
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
 }
 
 bool containsDuplicate(int* nums, int numsSize) {
