ce0fd0dcd1c51a08d2e269a8c597e08db1516473
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-19 18:29:38 -0500

    Time: 0 ms (100%), Space: 9.4 MB (35.46%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.c
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.c
@@ -2,28 +2,25 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* numbers, int numbersSize, int target, int* returnSize) {
-    int* ans = (int*)calloc(2, sizeof(int));
+    int* ans = calloc(2, sizeof(int));
+    *returnSize = 2;
 
-    for (int i=0; i<numbersSize; ++i) {
-        int l=i+1, r=numbersSize-1, remain=target-numbers[i];
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (numbers[mid] == remain) {
-                ans[0] = i+1;
-                ans[1] = mid+1;
+    int l=0, r=numbersSize-1;
+    while (l < r) {
+        int sum = numbers[l] + numbers[r];
+        if (sum == target) {
+            ans[0] = l+1;
+            ans[1] = r+1;
 
-                *returnSize = 2;
-                return ans;
-            }
-            else if (numbers[mid] > remain) {
-                r = mid - 1;
-            }
-            else {
-                l = mid + 1;
-            }
+            return ans;
+        }
+        else if (sum > target) {
+            r--;
+        }
+        else {
+            l++;
         }
     }
 
-    *returnSize = 2;
     return ans;
 }
