a9e69a5c5cafb44cdaf21e52ae3242b07f8608c1
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-19 10:25:05 -0500

    Time: 70 ms (6.65%), Space: 8.3 MB (47.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.c
+++ 0242-valid-anagram/0242-valid-anagram.c
@@ -1,12 +1,12 @@
 bool isAnagram(char* s, char* t) {
+    // Return False if the string length are different
     if (strlen(s) != strlen(t))
         return false;
 
     int count[26] = {0};
-
     for (int i=0; i<strlen(s); ++i) {
-        count[s[i]-'a']++;
-        count[t[i]-'a']--;
+        count[s[i] - 'a']++;
+        count[t[i] - 'a']--;
     }
 
     for (int i=0; i<26; ++i) {
