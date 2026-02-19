ed255b0eca1b7bd24f0e0df450d4852afbd7f16f
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-19 14:45:21 -0500

    Time: 0 ms (100%), Space: 8.8 MB (12.65%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -0,0 +1,17 @@
+bool isPalindrome(char* s) {
+    int l=0, r=strlen(s)-1;
+    while (l < r) {
+        while (l < r && !isalnum(s[l])) {
+            l++;
+        }
+        while (l < r && !isalnum(s[r])) {
+            r--;
+        }
+        if (tolower(s[l]) != tolower(s[r]))
+            return false;
+        l++;
+        r--;
+    }
+
+    return true;
+}
