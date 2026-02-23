a0b6df14349ca54a5053c75dec0385ec2a88f9d0
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-23 08:05:31 -0500

    Time: 0 ms (100%), Space: 14.1 MB (75.45%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-
-    void backtrack(vector<int>& candidates, int i, int remain, vector<int>& subset) {
+    void backtrack(vector<int>& candidates, int i, int remain, vector<int>& subset, vector<vector<int>>& ans) {
         if (remain == 0) {
             ans.push_back(subset);
             return;
         }
 
-        if (remain < 0 || i == candidates.size()) {
+        if (i == candidates.size() || remain < 0) {
             return;
         }
 
         subset.push_back(candidates[i]);
-        backtrack(candidates, i, remain-candidates[i], subset);
+        backtrack(candidates, i, remain-candidates[i], subset, ans);
         subset.pop_back();
-        backtrack(candidates, i+1, remain, subset);
+        backtrack(candidates, i+1, remain, subset, ans);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
         vector<int> subset;
-        backtrack(candidates, 0, target, subset);
 
+        backtrack(candidates, 0, target, subset, ans);
         return ans;
     }
 };
