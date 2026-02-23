66affd5f8c45f1021ab5078d822db6efa018221e
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-22 23:10:24 -0500

    Time: 283 ms (6.93%), Space: 159.9 MB (5.11%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void backtrack(int n, int k, int i, vector<int> subset) {
+        if (subset.size() == k) {
+            ans.push_back(subset);
+            return;
+        }
+
+        if (i > n) {
+            return;
+        }
+
+        for (int j=i; j<=n; ++j) {
+            subset.push_back(j);
+            backtrack(n, k, j+1, subset);
+            subset.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> subset;
+        backtrack(n, k, 1, subset);
+        return ans;
+
+    }
+};
