2eef16578d4d1276f6f118997b622f35dc1f6a1f
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-23 08:26:34 -0500

    Time: 51 ms (46.52%), Space: 29 MB (90.64%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,35 +2,30 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        // sorte the nums
-        std::sort(nums.begin(), nums.end());
 
-        // Treat it as a two sum problem
-        for (int i=0; i<nums.size()-2; i++)
-        {
-            // If the target is the same as the previous index, skip it
-            if (i-1>=0 && nums[i]==nums[i-1])
+        sort(nums.begin(), nums.end());
+        for (int i=0; i<nums.size(); ++i) {
+            if (i > 0 && nums[i] == nums[i-1])
                 continue;
-
-            int target=-nums[i], l=i+1, r=nums.size()-1;
-            while (l<r)
-            {
-                // if the goal is found
-                if (nums[l]+nums[r]==target)
-                {
-                    ans.push_back(vector<int>{nums[i], nums[l], nums[r]});
+                
+            int l=i+1, r=nums.size()-1;
+            while (l < r) {
+                int tmp = nums[i] + nums[l] + nums[r];
+                if (tmp == 0) {
+                    ans.push_back({nums[i], nums[l], nums[r]});
                     l++;
-                    r--;
-                    // prevent existing same pairs
-                    while (l<r && nums[l]==nums[l-1])
+                    while (l < r && nums[l] == nums[l-1])
                         l++;
-                    while (l<r && nums[r]==nums[r+1])
+                    r--;
+                    while (l < r && nums[r] == nums[r+1])
                         r--;
                 }
-                else if (nums[l]+nums[r]>target)
+                else if (tmp > 0) {
                     r--;
-                else
+                }
+                else {
                     l++;
+                }
             }
         }
 
