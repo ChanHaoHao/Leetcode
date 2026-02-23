9ce8b84314eee3e6479e7491671a99fffa1cbd3f
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-22 23:39:39 -0500

    Time: 3 ms (27.51%), Space: 11.2 MB (21.15%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-
-    void backtrack(vector<int>& nums, vector<bool>& used, vector<int>& subset) {
-        if (subset.size() == nums.size()) {
-            ans.push_back(subset);
-            return;
-        }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans = {{}};
 
         for (int i=0; i<nums.size(); ++i) {
-            if (!used[i]) {
-                subset.push_back(nums[i]);
-                used[i] = true;
-                backtrack(nums, used, subset);
-                used[i] = false;
-                subset.pop_back();
+            vector<vector<int>> new_perms;
+            for (auto perm: ans) {
+                for (int it=0; it<=perm.size(); ++it) {
+                    vector<int> p = perm;
+                    p.insert(p.begin()+it, nums[i]);
+                    new_perms.push_back(p);
+                } 
             }
+            ans = new_perms;
         }
-    }
 
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> subset;
-        vector<bool> used(nums.size(), false);
-
-        backtrack(nums, used, subset);
         return ans;
     }
 };
