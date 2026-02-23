afac9820a465c55ee8940df5ac0f2208a30fee04
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-02-22 23:25:33 -0500

    Time: 0 ms (100%), Space: 19.8 MB (7.74%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,16 +2,21 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
 
-        def backtrack(nums, subset):
+        def backtrack(nums, subset, used):
             if len(subset) == len(nums):
                 ans.append(subset.copy())
                 return
             
             for i in nums:
-                if i not in subset:
-                    subset.append(i)
-                    backtrack(nums, subset)
-                    subset.pop()
+                if i in used:
+                    continue
+                
+                used.add(i)
+                subset.append(i)
+                backtrack(nums, subset, used)
+                subset.pop()
+                used.remove(i)
+
         
-        backtrack(nums, [])
+        backtrack(nums, [], set())
         return ans
