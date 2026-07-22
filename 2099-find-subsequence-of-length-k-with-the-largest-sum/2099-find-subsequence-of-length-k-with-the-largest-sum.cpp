86bd0c83ce2b002e9188816764c8dcbe9f41c633
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-07-21 23:08:28 -0700

    Time: 2 ms (48.95%), Space: 13.9 MB (48.67%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
-        std::vector<int> sortedNums = nums;
-        std::sort(sortedNums.rbegin(), sortedNums.rend());
+        std::priority_queue<pair<int, int>, std::vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-        std::unordered_map<int, int> countMap;
-        for (int i=0; i<k; ++i) {
-            countMap[sortedNums[i]]++;
+        for (int i=0; i<nums.size(); ++i) {
+            pq.push({nums[i], i});
+
+            if (pq.size() > k) {
+                pq.pop();
+            }
         }
 
+        std::vector<pair<int, int>> topK;
+        while (!pq.empty()) {
+            topK.push_back(pq.top());
+            pq.pop();
+        }
+
+        std::sort(topK.begin(), topK.end(), [](const pair<int, int>& a, const pair<int, int>& b) { 
+            return a.second < b.second;
+        });
         std::vector<int> ans;
-        for (int n: nums) {
-            if (countMap[n] > 0) {
-                ans.push_back(n);
-                countMap[n] -= 1;
-            }
+        for (const auto& k: topK) {
+            ans.push_back(k.first);
         }
 
         return ans;
