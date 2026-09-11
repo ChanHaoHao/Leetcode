96b8032935b09ff40c8f1e0772bd12d3694c480b
Hao-Yu, Chan <70196922+ChanHaoHao@users.noreply.github.com>
2026-09-11 10:44:39 -0700

    Time: 0 ms (100%), Space: 28.7 MB (93.75%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -9,24 +9,16 @@ public:
 
         int ans = 0;
         for (int i=1; i<10; ++i) {
+            if (freq[i] == 0) continue;
             freq[i]--;
-
-            if (freq[i] >= 0) {
-                for (int j=0; j<10; ++j) {
-                    freq[j]--;
-
-                    if (freq[j] >= 0) {
-                        for (int k=0; k<10; k=k+2) {
-                            freq[k]--;
-
-                            if (freq[k] >= 0) {
-                                ans++;
-                            }
-                            freq[k]++;
-                        }
-                    }
-                    freq[j]++;
+            for (int j=0; j<10; ++j) {
+                if (freq[j] == 0) continue;
+                freq[j]--;
+                for (int k=0; k<10; k=k+2) {
+                    if (freq[k] == 0) continue;
+                    ans++;
                 }
+                freq[j]++;
             }
             freq[i]++;
         }
